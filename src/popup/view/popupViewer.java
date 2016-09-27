528519bbfb268a5bdf0ef620892bfc53037f2d26
Phuoctran2143 <phuoc.tran23@gmail.com>
2016-09-27 08:36:47 -0600

    created a input popup response

--- src/popup/view/popupViewer.java
+++ src/popup/view/popupViewer.java
@@ -13,5 +13,19 @@ public class popupViewer
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
+	
+	/**
+	 * Creates a input popup to collect a response from the user as a String
+	 * @param question The Quest being asked to the user.
+	 * @return The users response.
+	 */
 
+	public String collectResponse(String question)
+	{
+		String response = "";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
 }
